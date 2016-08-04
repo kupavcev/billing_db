44373c12a4b926453d445d202d14b282dacbda35
kupavcev <kupavcev@unact.ru>
2016-08-04 10:20:39 +0300

    Update tables.sql

--- aws/tables.sql
+++ aws/tables.sql
@@ -1,4 +1,6 @@
 CREATE TABLE aws.aws_billing_reports_detailed(
+	id bigint primary key IDENTITY(1,1),
+	billing_date datetime not null,
 	InvoiceID varchar(50) NULL,
 	PayerAccountId varchar(50) NULL,
 	LinkedAccountId varchar(50) NULL,
@@ -12,7 +14,7 @@ CREATE TABLE aws.aws_billing_reports_detailed(
 	Operation varchar(50) NULL,
 	AvailabilityZone varchar(50) NULL,
 	ReservedInstance varchar(50) NULL,
-	ItemDescription text NULL,
+	ItemDescription varchar(1024) NULL,
 	UsageStartDate varchar(50) NULL,
 	UsageEndDate varchar(50) NULL,
 	UsageQuantity varchar(50) NULL,
@@ -20,5 +22,15 @@ CREATE TABLE aws.aws_billing_reports_detailed(
 	BlendedCost varchar(50) NULL,
 	UnBlendedRate varchar(50) NULL,
 	UnBlendedCost varchar(50) NULL,
-	ResourceId varchar(max) NULL,
-	user Name varchar(max) NULL) ON aws;
+	ResourceId varchar(255) NULL,
+	userName varchar(512) NULL
+) ON aws
+
+--drop table aws.aws_billing_reports_detailed;
+
+create index aws_billing_reports_detailed_billing_date on aws.aws_billing_reports_detailed(billing_date);
+create index aws_billing_reports_detailed_ResourceId on aws.aws_billing_reports_detailed(ResourceId);
+create index aws_billing_reports_detailed_UsageStartDate  on aws.aws_billing_reports_detailed(UsageStartDate);
+create index aws_billing_reports_detailed_UsageEndDate  on aws.aws_billing_reports_detailed(UsageEndDate);
+create index aws_billing_reports_detailed_userName  on aws.aws_billing_reports_detailed(userName);
+--create index aws_billing_reports_detailed_  on aws.aws_billing_reports_detailed( );
