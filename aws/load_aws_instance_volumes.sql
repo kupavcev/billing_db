0484dcb477d2d4217ab49915a9c8df621952fe55
kupavcev <kupavcev@unact.ru>
2016-08-23 14:33:02 +0300

    Create load_aws_instance_volumes.sql

---
+++ aws/load_aws_instance_volumes.sql
@@ -0,0 +1,17 @@
+create table #aws_instance_volumes_tmp(
+	instanceId varchar(255),
+	instanceTag varchar(1024),
+	volumeId varchar(255));
+
+
+BULK INSERT #aws_instance_volumes_tmp
+    FROM 'C:\AWS_Billing_Reports\Data\aws_instance_info_nc.txt'
+    WITH
+    (
+    FIRSTROW = 2,
+    FIELDTERMINATOR = ',',  --CSV field delimiter
+    ROWTERMINATOR = '0x0a',   --Use to shift the control to next row
+    ERRORFILE = 'C:\AWS_Billing_Reports\Data\aws_instance_info_nc.txt.err'--,
+    );	
+
+select * from #aws_instance_volumes_tmp;
