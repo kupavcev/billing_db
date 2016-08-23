856b0c4782bf72a624548f82c7121327d7216f0f
kupavcev <kupavcev@unact.ru>
2016-08-23 15:05:03 +0300

    Update load_aws_instance_volumes.sql

--- aws/load_aws_instance_volumes.sql
+++ aws/load_aws_instance_volumes.sql
@@ -1,3 +1,4 @@
+drop table #aws_instance_volumes_tmp
 create table #aws_instance_volumes_tmp(
 	instanceId varchar(255),
 	instanceTag varchar(1024),
@@ -9,9 +10,15 @@ BULK INSERT #aws_instance_volumes_tmp
     WITH
     (
     FIRSTROW = 2,
-    FIELDTERMINATOR = ',',  --CSV field delimiter
+    FIELDTERMINATOR = ';',  --CSV field delimiter
     ROWTERMINATOR = '0x0a',   --Use to shift the control to next row
     ERRORFILE = 'C:\AWS_Billing_Reports\Data\aws_instance_info_nc.txt.err'--,
     );	
 
-select * from #aws_instance_volumes_tmp;
+--truncate table aws.aws_instances;
+insert into aws.aws_instances(instanceId,instanceTag)
+select distinct instanceId, instanceTag from #aws_instance_volumes_tmp;
+
+--truncate table aws.aws_volumes;
+insert into aws.aws_volumes(volumeId)
+select distinct volumeId from #aws_instance_volumes_tmp
