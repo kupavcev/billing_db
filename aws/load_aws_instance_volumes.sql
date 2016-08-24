1b02c374e8624f4a11fecfd48228262761581af4
kupavcev <kupavcev@unact.ru>
2016-08-24 07:05:20 +0300

    Update load_aws_instance_volumes.sql

--- aws/load_aws_instance_volumes.sql
+++ aws/load_aws_instance_volumes.sql
@@ -10,15 +10,24 @@ BULK INSERT #aws_instance_volumes_tmp
     WITH
     (
     FIRSTROW = 2,
-    FIELDTERMINATOR = ';',  --CSV field delimiter
+    FIELDTERMINATOR = ';|;',  --CSV field delimiter
     ROWTERMINATOR = '0x0a',   --Use to shift the control to next row
     ERRORFILE = 'C:\AWS_Billing_Reports\Data\aws_instance_info_nc.txt.err'--,
     );	
 
 --truncate table aws.aws_instances;
 insert into aws.aws_instances(instanceId,instanceTag)
-select distinct instanceId, instanceTag from #aws_instance_volumes_tmp;
+select distinct instanceId, instanceTag from #aws_instance_volumes_tmp
+where instanceId not in(select instanceId from #aws_instance_volumes_tmp);
+
+update aws.aws_instances set aws.aws_instances.instanceTag=#aws_instance_volumes_tmp.instanceTag
+from #aws_instance_volumes_tmp
+where aws.aws_instances.instanceId=#aws_instance_volumes_tmp.instanceId and aws.aws_instances.instanceTag<>#aws_instance_volumes_tmp.instanceTag;
 
 --truncate table aws.aws_volumes;
 insert into aws.aws_volumes(volumeId)
 select distinct volumeId from #aws_instance_volumes_tmp
+where volumeId not in(select volumeId from #aws_instance_volumes_tmp);
+
+
+select * from aws.aws_instances
