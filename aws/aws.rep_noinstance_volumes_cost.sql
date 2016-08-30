4a554c21a3f4b811a68ec5b5ee51427d60ff7b13
kupavcev <kupavcev@unact.ru>
2016-08-30 09:27:30 +0300

    Update aws.rep_noinstance_volumes_cost.sql

--- aws/aws.rep_noinstance_volumes_cost.sql
+++ aws/aws.rep_noinstance_volumes_cost.sql
@@ -15,7 +15,7 @@ if month(@ddate)<10
 set @ddate='01.'+@month+'.'+@year;
 
 
-select ResourceId as volumeId,ItemDescription as rDescription,username as volumeTag,sum(BlendedCost) as Cost 
+select ResourceId as volumeId,ItemDescription as rDescription,username as volumeTag,sum(unBlendedCost) as Cost 
 from aws.aws_billing_reports_detailed
 	where LinkedAccountId='470567614800' and billing_date=@ddate 
 	--and ResourceId is null 
@@ -24,6 +24,6 @@ from aws.aws_billing_reports_detailed
 	and (ResourceId not in(select volumeId from aws.aws_volumes) and 
 	ResourceId not in(select instanceId from aws.aws_instances))
 	group by ResourceId, userName, 	ItemDescription, username
-	having sum(BlendedCost)>0
-	order by sum(BlendedCost) desc
+	having sum(unBlendedCost)>0
+	order by sum(unBlendedCost) desc
 end;
